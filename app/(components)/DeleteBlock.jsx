e5f82cdd2fdd05c9b0bcdfd301306e4d17f6dcc0
Abhishek Kumar <ak3037308@gmail.com>
2024-01-16 12:13:25 +0530

    first commit

---
+++ app/(components)/DeleteBlock.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+   let text = " Are you Sure, You want to delete this ticket!!!!!"
+    if(confirm(text) === true ){
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  }
+   
+  };
+
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className=" text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
+  );
+};
+
+export default DeleteBlock;
---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,26 @@
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className="flex flex-row justify-around bg-nav p-4">
+      <div className="">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        </div>
+        <div>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+        </div>
+      
+      <div>
+        <p className=" text-default-text">test@gmail.com</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
